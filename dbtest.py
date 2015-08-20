f7ab65d1c89b851dece89360f13d9a39fca1a986
Firas S <choogiesaur@gmail.com>
2015-08-20 11:10:13 -0400

    create alert_rteadv
    preliminary

--- dbtest.py
+++ dbtest.py
@@ -1,7 +1,14 @@
 from __future__ import division
-import cx_Oracle
 from datetime import datetime, timedelta
+from tabulate import tabulate
+import cx_Oracle
 import smtplib
+
+"""
+NOTE: This was primarily made with Python 3 in mind. It works on vetools01 with python 2 and 
+the __future__ import, but if any compatibility issues arise, just install the above packages
+and run on python 3.
+"""
 
 #ex01-scan.prod-idt.net 	<- host
 #ex02-scan.prod.idt.net
@@ -22,7 +29,7 @@ def print_fields(cursor):
 		print(row[0])
 	print()
 
-#takes a cx_Oracle cursor object and prints the rows associated with high packet loss (hpl)
+#takes a cx_Oracle cursor object and prints the rows associated with high packet loss (hpl) DEPRECATED/TESTING
 def print_hpl_rows(cursor):
 	count = 0
 	#print some example rows
@@ -63,10 +70,12 @@ def gen_hpl_alert(offenders):
 	msg = 'Alert: High packet loss on greater than 15% of calls from the following ' + str(len(offenders)) + ' trunks:\n'
 
 	#sort by percentage. switch to tup[0] to sort by trunk name
-	offenders.sort(key=lambda tup: tup[1])
+	offenders.sort(key=lambda tup: tup[3])
 
-	for pair in offenders:
-		msg += "\ntrunk name: " + str(pair[0]) + "\npercentage: " + "%.2f%%\n" % pair[1]
+	#for row in offenders:
+	#	msg += "\ntrunk name: " + str(row[0]) + "\npercentage: " + "%.2f%%\n" % row[3]
+
+	msg += '\n' + tabulate(offenders, headers=['Trunk','Completed Calls','Total HPL Calls','Percentage'])
 
 	return msg
 
@@ -98,10 +107,11 @@ def send_email(msg, recipients):
 
 	server.quit()
 
-#takes a cx_Oracle cursor object and prints list of tg_id's with HPL above threshold. Also takes current 
+#takes a cx_Oracle cursor object and prints list of trunks with HPL above threshold.
 def alert_pktloss(cursor):
 	
-	recipients = ['firas.sattar@idt.net', 'traffic.summarizer.alerts@gmail.com', 'romel.khan@idt.net']
+	recipients = ['firas.sattar@idt.net', 'traffic.summarizer.alerts@gmail.com']
+	#recipients = ['firas.sattar@idt.net', 'traffic.summarizer.alerts@gmail.com', 'romel.khan@idt.net', 'richard.lee@idt.net']
 
 	#list of trunks with HPL on 15% or more of calls
 	offenders 	= []
@@ -126,12 +136,37 @@ def alert_pktloss(cursor):
 			
 			#if 15% or more of calls have HPL, add to offenders list
 			if (total_hlpkt_calls / completed) >= 0.15:
-				offenders.append([trunk, (total_hlpkt_calls/completed) * 100])
+				offenders.append([trunk, completed, total_hlpkt_calls, (total_hlpkt_calls/completed) * 100])
 
-	#print list of offenders
+	#print alert to terminal, then send email to recipients
 	alert = gen_hpl_alert(offenders)
 	print(alert)
 	send_email(alert, recipients)
+
+#takes a cx_Oracle cursor object and prints list of tg_id's with HPL above threshold. Also takes current 
+def alert_rteadv(cursor):
+	
+	recipients = ['firas.sattar@idt.net', 'traffic.summarizer.alerts@gmail.com']
+
+	#list of trunks with HPL on 15% or more of calls
+	offenders 	= []
+
+	#calculate time from which records will be fetched. OS time => GMT => 2 hours before => floored to last hour
+	timeframe = get_timeframe(datetime.now())
+
+	for row in cursor:
+
+		date 				= row[0] #save as datetime obj, not string
+		trunk 				= row[1]
+		direction 			= str(row[2])
+		attempts 			= row[3]
+		answered			= row[4]
+		failed				= row[5]
+		tdra_count			= row[8]
+		tdra_total			= row[9]
+		tdra_avg			= row[10]
+
+		print(row)
 
 """------------"""
 """MAIN PROGRAM"""
@@ -151,11 +186,12 @@ db 		= cx_Oracle.connect('OSSREAD', 'oss2002read', dsn_tns)
 #create a cursor object; basically an iterator for select queries.
 curs 	= db.cursor()
 
-#fetch rows to be examined
-curs.execute('SELECT * FROM ossdb.v_tg_pkt_loss ORDER BY tstamp')
+#fetch rows to be examined then perform the High Packet Loss check
+#curs.execute('SELECT * FROM ossdb.v_tg_pkt_loss ORDER BY tstamp')
+#alert_pktloss(curs)
 
-#print_fields(curs)
-#print_hpl_rows(curs)
-alert_pktloss(curs)
+#fetch rows to be examined then perform the route advanceable check
+curs.execute('SELECT * FROM ossdb.v_tg_tdra ORDER BY tdra_avg DESC')
+alert_rteadv(curs)
 
 db.close()
